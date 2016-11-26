aa455201a931fa25816d0a2193e79835a084c3f5
snrao310 <sameerana.nrao@yahoo.com>
2016-11-26 08:17:37 -0700

    done

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import re
 from bs4 import BeautifulSoup
 import urllib.request
 
-with open('ques.json', 'r') as f:
+with open('webmd-question.json', 'r') as f:
     json_data = f.read()
     json_data = json_data.replace("\\'", "'")
     json_data=json_data.replace('\t', ' ')
@@ -12,34 +12,49 @@ data = json.loads(json_data)
 
 
 
-print (data[0])
+# print (data[0])
 
-print (data[0]['questionURL'])
-with open('out.json', 'w') as outfile:
-    outfile.write('[')
+# print (data[0]['questionURL'])
+# with open('out.json', 'w') as outfile:
+#     outfile.write('[')
 
 first=0
+count=0
 
 for jsonobj in data:
+    k=0
     url=jsonobj['questionURL']
     page = urllib.request.Request(url, headers={'User-Agent' : "Magic Browser"})
-    html= urllib.request.urlopen( page )
-    soup = BeautifulSoup(html)
-    s=soup("div", {"class":"question-answer"})
+    try:
+        html= urllib.request.urlopen( page )
+        soup = BeautifulSoup(html)
+        s=soup("div", {"class":"question-answer"})
 
-    for foo in soup.find_all('div', {'class': 'question-answer'}):
-        bar = foo.find('span', {'class': 'date_posted_fmt'})
-        date=bar.text
-        i=date.index('\'')
-        j=date.rfind('\'')
+        for foo in soup.find_all('div', {'class': 'question-answer'}):
+            bar = foo.find('span', {'class': 'date_posted_fmt'})
+            date=bar.text
+            i=date.index('\'')
+            j=date.rfind('\'')
 
-        print(date[i+1:j])
-        jsonobj['questionPostDate']=date[i+1:j]
-        with open('out.json', 'a') as outfile:
-            if first != 0:
-                outfile.write(' , ')
-            json.dump(jsonobj, outfile)
-            first+=1
+            # print(date[i+1:j])
+            jsonobj['questionPostDate']=date[i+1:j]
+            count+=1
+            if(count % 100 == 0):
+                print(count, " done")
 
-with open('out.json', 'a') as outfile:
-    outfile.write(']')
+            # print (count)
+            # print (jsonobj)
+
+            # with open('out.json', 'a') as outfile:
+            #     if first != 0:
+            #         outfile.write(' , ')
+            #     json.dump(jsonobj, outfile)
+            #     first+=1
+
+    except Exception:
+        print (Exception)
+        continue
+
+
+with open('out.json', 'w') as outfile:
+    json.dump(data, outfile)
